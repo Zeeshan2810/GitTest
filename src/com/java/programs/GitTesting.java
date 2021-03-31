749c2558fa47b01761b4531065b21e59813d2e41
AHMED Zeeshan TGI/OLS <zeeshan.ahmed@orange.com>
2021-03-31 16:52:37 +0530

    Write some logical program

--- src/com/java/programs/GitTesting.java
+++ src/com/java/programs/GitTesting.java
@@ -4,6 +4,14 @@ public class GitTesting {
 
 	public static void main(String[] args) {
 		System.out.println("This is new changes in HomePage Branch");
+		int a = 10, b= 20 ,temp;
+		
+		temp = a;
+		a = b;
+		b= temp;
+		
+		System.out.println("values of a and b are interchanged: "+"a="+a+" and b= "+b);
+		
 
 	}
 
